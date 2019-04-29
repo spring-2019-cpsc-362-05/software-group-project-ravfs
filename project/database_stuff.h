555d5d6d1f74c69da19596b02a815da11c38a67b
sa5mmm <samdm@posteo.net>
2019-04-29 12:51:12 -0700

    Should have all prototypes now

--- project/database_stuff.h
+++ project/database_stuff.h
@@ -18,13 +18,26 @@ std::unique_ptr<Accounts::Account> get_account(const QString& username, const QS
 
 }
 /*Account database functions*/
-bool createAccount(const QString& firstName,const QString& lastName,const QString& email,const QString& username,const QString& password,const QString& mailingAddress,const QString& physicalAddress,const QString& phoneNumber);
+bool createAccount(const QString& firstName,const QString& lastName,
+                   const QString& email,const QString& username,const QString& password,
+                   const QString& mailingAddress,const QString& physicalAddress,const QString& phoneNumber);
+
 QString aGetFName(const QString& accountID);
 QString aGetLName(const QString& accountID);
 QString aGetEMail (const QString& accountID);
 QString aGetMAddress(const QString& accountID);
 QString aGetPAddress(const QString& accountID);
 QString aGetPNumber(const QString& accountID);
+
+
+QString aSetFName(const QString& accountID,const QString& valueToSet);
+QString aSetLName(const QString& accountID,const QString& valueToSet);
+QString aSetEMail (const QString& accountID,const QString& valueToSet);
+QString aSetMAddress(const QString& accountID,const QString& valueToSet);
+QString aSetPAddress(const QString& accountID,const QString& valueToSet);
+QString aSetPNumber(const QString& accountID,const QString& valueToSet);
+QString aSetPassword(const QString& accountID,const QString& valueToSet);
+
 
 /*Request database functions.*/
 QString rGetEFLag(const QString& requestID);
@@ -37,7 +50,11 @@ QString rGetProvider(const QString& requestID);
 QString rGetProviderContact(const QString& requestID);
 
 /*Service Provider database functions*/
-bool createServiceProvider(const QString& accountID,const QString& SPPhone,const QString& SPEmail,const QString& SPWebsite,const QString& SPNormalHours,const QString& SPEHours,const QString& SPServicesProvided,const QString& SPHourlyRate,const QString& SPBaseRate,const QString& SPLocationKey);
+bool createServiceProvider(const QString& accountID,const QString& SPPhone,
+                           const QString& SPEmail,const QString& SPWebsite,
+                           const QString& SPNormalHours,const QString& SPEHours,
+                           const QString& SPServicesProvided,const QString& SPHourlyRate,
+                           const QString& SPBaseRate,const QString& SPLocationKey);
 //These getter functions can possibly be used from other account types
 //ProviderID is just the accountID of the provider in question
 QString spGetPhone(const QString& ProviderID);
@@ -66,7 +83,6 @@ bool spAddLocationKey(const QString& accountID, const QString& valueToSet);
 //viewRequests
 bool spProcessRequest(const QString& accountID, const QString& requestID);
 bool spCreateAClaim(const QString& accountID, const QString& requestID, const QString& invoice);
-
 /*Tenant database functions*/
 bool createTenant(const QString& accountID,const QString& primaryLocation,const QString& locationKey);
 bool tSetLocationKey(const QString& accountID,const QString& locationKey);
@@ -78,8 +94,12 @@ bool tCancelRequest(const QString& accountID, const QString& requestID);
 
 
 /*Landlord database functions*/
-bool createLandlord(const QString& accountID, const QString& LLPhone, const QString& LLEmail, const QString& LLWebsite);
-bool llAddLocation(const QString& accountID, const QString& locHours, const QString& locAddress, const QString& locDays, const QString& locPhone, const QString& locEmail, const QString& locWebsite);
+bool createLandlord(const QString& accountID, const QString& LLPhone,
+                    const QString& LLEmail, const QString& LLWebsite);
+bool llAddLocation(const QString& accountID, const QString& locHours,
+                   const QString& locAddress, const QString& locDays,
+                   const QString& locPhone, const QString& locEmail,
+                   const QString& locWebsite);
 
 //view request
 bool llAcceptRequest(const QString& accountID, const QString& requestID);
@@ -89,13 +109,22 @@ QString llGetPhone(const QString& accountID);
 QString llGetLocationInfo(const QString& accountID, const QString& locationID);
 QString llGetLocationLink(const QString& accountID, const QString& locationID);
 QString llGetAllLocations(const QString& accountID);
-
+QString llPayClaim(const QString& claimID);
 
 //Service Provider, Tenant, and Landlord want to view all their requests!
 
 QString spGetAllRequests(const QString& providerAccountID);
 QString llGetAllRequests(const QString& landlordAccountID);
 QString tGetAllRequests(const QString& tenantAccountID);
+
+/*Claim database functions*/
+QString getClaimID(const QString& requestID);
+QString getClaimInvoice(const QString& claimID);
+QString getClaimProvider(const QString& claimID);
+QString getClaimFilledTime(const QString& claimID);
+QString getClaimPaidTime(const QString& claimID);
+QString getClaimStatus(const QString& claimID);
+QString setModifyClaimInvoice(const QString& claimID, const QString& valueToSet);
 
 
 
